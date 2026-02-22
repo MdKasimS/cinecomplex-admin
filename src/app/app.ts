04d5c5db854263fbad13a71b4f972ffcf8e6f403
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2026-02-23 00:05:01 +0530

    Registeration component is added. App is functional

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
-import { Footer } from "./footer/footer"; 
+import { Footer } from "./footer/footer";
+import { Register } from "./register/register"; 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Register],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/register/register.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as bootstrap from 'bootstrap';
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+
+  user = {
+    name: '',
+    email: '',
+    contact: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log('Form submitted:', this.user);
+    // TODO: Can we use here toast to show user registeration details instead of alert?
+    // Its available in used bootswatch theme. But I am not sure how to use it in angular. So, for now using alert.
+    // Show Bootstrap toast
+    const toastEl = document.getElementById('registerToast');
+    if (toastEl) {
+      const toast = new bootstrap.Toast(toastEl);
+      toast.show();
+    } 
+  }
+
+  onClear() {
+    this.user = { name: '', email: '', contact: '', password: '' };
+  }
+}
