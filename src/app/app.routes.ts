327d5467b42c4c521c2f84432a85dddf7aafc3ca
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2026-02-25 01:00:11 +0530

    Added Home, Login component. But again dropdwon failed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Register } from './register/register';
+import { Home } from './home/home';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // default route
+  { path: '**', redirectTo: '/login' }, // wildcard fallback
+];
